b779206d91a8234affd1d617508d986ac88aad07
Adia Hou <adiahoujy@gmail.com>
2024-11-01 11:55:11 +0800

    delete unuse file

--- Python/Stocks/config.py
+++
@@ -1,9 +0,0 @@
-tiger_id = '20153547'
-tiger_account = '20240605143234090'
-
-api_key_alpaca='PKBBK55XBNQ9M1JQV0MM'
-secret_key_alpaca='ygoS5Wzqf4FcXFsiXoxeAlI1TJcrCQBwmA4Kln8X'
-
-api_key_alpha='7CK85VCOJE11160Z'
-
-api_key_finnhub = 'cpl6rh1r01qulsvkjr50cpl6rh1r01qulsvkjr5g'
